3e8030b1e2d7b1c58d6c48f0f1e74096f303b744
theLahcim <michal.mazur32@gmail.com>
2022-11-28 21:14:13 +0100

    feat(Dogs): show dog results feat(Cats): show cat results chore: Flask

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from utils import detection, argparser
 import glob
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
 args = argparser()
 files = glob.glob('images/*.jpg')
 files.extend(glob.glob('images/*.png'))
-for f in files:
-    print(f)
-    detection(f, args["confidence"], args["detect"])
 
+
+class Dogs(Resource):
+    @staticmethod
+    def get():
+        d = {}
+        for i, f in enumerate(files):
+            d[i] = {}
+            print(f)
+            d[i] = detection(f, args["confidence"], 'dog')
+        return d
+
+
+class Cats(Resource):
+    @staticmethod
+    def get():
+        d = {}
+        for i, f in enumerate(files):
+            d[i] = {}
+            print(f)
+            d[i] = detection(f, args["confidence"], 'cat')
+        return d
+
+
+api.add_resource(Dogs, '/dogs')
+api.add_resource(Cats, '/cats')
+
+if __name__ == '__main__':
+    app.run(debug=True)
